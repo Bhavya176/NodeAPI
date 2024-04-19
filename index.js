b8e668ce8f3ecda508a030bb443a3257d6814326
Bhavya Savaliya <bhavya.savaliya@binaryrepublik.com>
2024-04-19 19:09:07 +0530

    change index file

--- index.js
+++ index.js
@@ -7,7 +7,9 @@ connectDb();
 const app = express();
 
 const port = process.env.PORT || 5000;
-
+app.get("/", (req, res) => {
+  res.send("products api running new deploy");
+});
 app.use(express.json());
 app.use("/api/contacts", require("./routes/contactRoutes"));
 app.use("/api/users", require("./routes/userRoutes"));
