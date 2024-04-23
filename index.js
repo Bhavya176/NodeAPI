6786b71ecf442406e461b49446479d0cab80b6bd
Bhavya Savaliya <bhavya.savaliya@binaryrepublik.com>
2024-04-23 16:40:11 +0530

    add ejs files

--- index.js
+++ index.js
@@ -3,14 +3,24 @@ const connectDb = require("./config/dbConnection");
 const errorHandler = require("./middleware/errorHandler");
 const dotenv = require("dotenv").config();
 const productRoutes = require("./routes/productRoutes");
-
 connectDb();
 const app = express();
-
+app.set("view engine", "ejs");
 const port = process.env.PORT || 5000;
 app.get("/", (req, res) => {
-  res.send("products api running new deploy");
+  // res.send("products api running new deploy");
+  res.render("home");
 });
+// app.get("/register", (req, res) => {
+//   res.render("registerPage");
+// });
+// app.get("/login", (req, res) => {
+//   res.render("loginPage");
+// });
+app.get("/about", (req, res) => {
+  res.render("aboutPage");
+});
+
 app.use(express.json());
 app.use("/products", productRoutes);
 app.use("/contacts", require("./routes/contactRoutes"));
